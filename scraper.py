ffc4dd9dbf6b1d2265c82d3e93eca69c9998c787
Joerg Henning <henning.joerg@gmail.com>
2016-03-10 21:54:36 +0800

    Ingore track location of value is empty

--- scraper.py
+++ scraper.py
@@ -171,7 +171,7 @@ def parse_row(row, last_speaker, last_session, current_track):
     if not hasattr(session, 'track'):
         session.track = {'id': track.id, 'name': track.name}
     if not hasattr(session, 'location'):
-        if row.has_key('Location'):
+        if row.has_key('Location') and row['Location']:
             session.location = row['Location']
         else:
             session.location = track.location
