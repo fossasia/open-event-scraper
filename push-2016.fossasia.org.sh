d23932e0c63f95cb7153c9c47932b78b1ad56cbc
Tay Ray Chuan <rctay89@gmail.com>
2016-03-13 07:32:20 +0000

    travis: push generated result to 2016.fossasia.org

---
+++ push-2016.fossasia.org.sh
@@ -0,0 +1,12 @@
+# thanks to https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+#!/bin/sh
+set -e
+
+git clone "https://${GH_TOKEN}@github.com/fossasia/2016.fossasia.org" repo_out
+cp schedule/index.html repo_out/schedule/index.html
+cd repo_out
+git config user.name "Travis CI"
+git config user.email "noreply-travis@fossasia.org"
+git commit -m 'Regenerated schedule/index.html' schedule/index.html
+git push origin gh-pages
+
