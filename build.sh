a8b74a71db3ffe38d1a631d2c68ca5182dc5d2bc
Travis CI <noreply+travis@fossasia.org>
2016-03-13 16:42:56 +0000

    Revert "try dropping || check when committing"
    This reverts commit 8fcdc06c30ec36b821be9696f02c54500b174fdd.

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ git config --global user.email "noreply+travis@fossasia.org"
 python scraper.py
 
 # don't continue if no changes
-if git diff-index --quiet HEAD -- out/*.json; then
+if git diff-index --quiet HEAD; then
   exit 0
 fi
 
-git commit -m '[Auto] updated json files [ci skip]' out/*.json
+git commit -m '[Auto] updated json files [ci skip]' out/*.json || echo "no changes"
 git push "https://${GH_TOKEN}@github.com/fossasia/open-event-scraper" HEAD:master
 
 git clone --depth=1 "https://${GH_TOKEN}@github.com/fossasia/2016.fossasia.org" fa16-repo
@@ -21,11 +21,7 @@ node schedule/generator > fa16-repo/schedule/index.html
 
 cd fa16-repo
 
-if git diff-index --quiet HEAD -- schedule/index.html; then
-  exit 0
-fi
-
-git commit -m '[Auto] updated schedule' schedule/index.html
+git commit -m '[Auto] updated schedule' schedule/index.html || echo "no changes"
 git push origin gh-pages
 
 exit 0
