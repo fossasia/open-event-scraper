d36e08edacf386841224c187627551bc9c1d89da
Tay Ray Chuan <rctay89@gmail.com>
2016-03-13 18:40:11 +0800

    check for changes before pushing schedule html

--- push-2016.fossasia.org.sh
+++ push-2016.fossasia.org.sh
@@ -2,11 +2,12 @@
 #!/bin/sh
 set -e
 
-git clone "https://${GH_TOKEN}@github.com/fossasia/2016.fossasia.org" repo_out
-cp schedule/index.html repo_out/schedule/index.html
-cd repo_out
-git config user.name "Travis CI"
-git config user.email "noreply-travis@fossasia.org"
-git commit -m 'Regenerated schedule/index.html' schedule/index.html
-git push origin gh-pages
-
+if !git diff-index --quiet HEAD schedule/index.html; then
+  git clone "https://${GH_TOKEN}@github.com/fossasia/2016.fossasia.org" repo_out
+  cp schedule/index.html repo_out/schedule/index.html
+  cd repo_out
+  git config user.name "Travis CI"
+  git config user.email "noreply-travis@fossasia.org"
+  git commit -m 'Regenerated schedule/index.html' schedule/index.html
+  git push origin gh-pages
+fi
